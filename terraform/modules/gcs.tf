5c1f863fb26fb54d698393fd7911288021817ec2
daichi_yamaoka <tnychor06@gmail.com>
2022-11-04 14:45:47 +0900

    update gcs name

--- terraform/modules/gcs.tf
+++ terraform/modules/gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "test_bucket" {
-  name          = "sample-bucket"
+  name          = "${var.project_id}-sample-bucket"
   location      = var.region
   storage_class = "REGIONAL"
 }
